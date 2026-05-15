03d58eb749def9f096f5685267558c3f3f0dbd62
Jay Ganesh Chankhore <jaychankhore08@gmail.com>
2026-05-15 18:48:08 +0530

    Update gene_dot_plot.code.R

--- BEE102 ass/gene_onotology_analysis/gene_dot_plot.code.R
+++ BEE102 ass/gene_onotology_analysis/gene_dot_plot.code.R
@@ -1,15 +1,29 @@
-library(clusterProfiler)
-library(org.Hs.eg.db)
-genes = read.table("gene_list_tss_upstream_with_pattern/optimal_site_nrf1_in_hg38_tss_upstream_500.genes.tsv", 
-                   sep = "\t", header = F, stringsAsFactors = F)$V1
-ego <- enrichGO(gene         = genes,
-                OrgDb        = org.Hs.eg.db,
-                keyType      = "SYMBOL",
-                ont          = "BP",
-                pAdjustMethod = "BH",
-                qvalueCutoff = 0.01)
+genes = read.table(
+  "/home/userjay_chankhore/my_nrf1_genes.txt",
+  header = FALSE,
+  stringsAsFactors = FALSE
+)$V1
 
+# GO enrichment
+ego <- enrichGO(
+  gene = genes,
+  OrgDb = org.Hs.eg.db,
+  keyType = "SYMBOL",
+  ont = "BP",
+  pAdjustMethod = "BH",
+  qvalueCutoff = 0.01
+)
+
+# Save results
 write.csv(as.data.frame(ego), "GO_annotation_results.csv")
-pdf ("dotplot.pdf", height = 8, width = 8)
-dotplot(ego, showCategory =20, font.size = 6)
+
+# Create PNG plot
+png("dotplot.png", width = 1200, height = 1200)
+
+dotplot(
+  ego,
+  showCategory = 20,
+  font.size = 8
+)
+
 dev.off()
